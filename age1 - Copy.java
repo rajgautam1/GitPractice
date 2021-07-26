ebe8f464b349103054cd2e28d52607cc32674a95
raj <rajgautam98456@gmail.com>
2021-07-26 14:44:08 -0400

    Created new Branch 2:44

---
+++ age1 - Copy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class age1 {
+
+	public static void main(String[] args) {
+		System.out.println("How old are you ?");
+Scanner scan = new Scanner(System.in);
+int age = scan.nextInt();
+
+System.out.println("Age : "  + age);
+		System.out.println(" What is your date of birth?")
+	}
+
+}
